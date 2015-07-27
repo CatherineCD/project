1b740472e8535a58f33958cf423ffd2e971f729a
dotfinal <admin@dotfinal.ru>
2015-07-27 13:11:43 +0600

    Updating

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=project',
-    'username' => 'root',
-    'password' => 'black', // black
-    'charset' => 'utf8',
-];
